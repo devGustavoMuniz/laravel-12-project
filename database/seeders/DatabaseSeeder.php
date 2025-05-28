a6b5e1f381729d29f2c8dda49cb89cf68d4598d5
Muniz <gustavomuniz545@gmail.com>
2025-05-28 20:17:28 -0300

    teste 4

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,10 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'is_admin' => true,
         ]);
+
+        User::factory(5)->create();
 
         $this->call([
             AuthorSeeder::class
